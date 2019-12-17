33ddb6e8633906db52e881ff3e7222e67cb94f74
Egor Zuev <zyev.egor@rambler.ru>
2019-12-17 18:15:59 +0300

    *task 8 - json

--- hw08-json/src/test/java/hw08/MainTest.java
+++ hw08-json/src/test/java/hw08/MainTest.java
@@ -1,25 +1,40 @@
-package ru.otus.main;
+package hw08;
+
 
 import com.google.gson.Gson;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import ru.otus.jsoner.Jsoner;
 
-public class Main {
+public class MainTest {
 
-    public static void main(String[] args){
+    private Gson gson;
+    private Jsoner jsoner;
+
+    @Before
+    public void init() {
+        this.gson = new Gson();
+        this.jsoner = new Jsoner();
+
+    }
+
+
+    @Test
+    public void checkToJSON(){
 
         Gson gson = new Gson();
         StrangeObject strangeObject = new StrangeObject(1, "test", 12f);
         strangeObject.addToList(188);
         strangeObject.addToMap("super_key", true);
         String json = gson.toJson(strangeObject);
-
-        System.out.println(json);
-
+        
         Jsoner jsoner = new Jsoner();
 
         String json2 = jsoner.toJSON(strangeObject);
 
-       System.out.println("are equal: " + json.equals(json2));
+        Assert.assertEquals(json, json2);
+
     }
 
 
--- hw08-json/src/test/java/hw08/StrangeObject.java
+++ hw08-json/src/test/java/hw08/StrangeObject.java
@@ -1,4 +1,4 @@
-package ru.otus.main;
+package hw08;
 
 import java.util.ArrayList;
 import java.util.HashMap;
